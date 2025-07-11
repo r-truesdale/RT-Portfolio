32106e15063bfb436a19f9f6acad7deddb68189f
Rebecca Truesdale <rebecca.truesdale@gmail.com>
2025-07-11 14:22:24 +0100

    proj page update

--- react-portfolio/src/pages/Projects.jsx
+++ react-portfolio/src/pages/Projects.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { Tabs, Tab } from 'react-bootstrap';
 import styles from "./Projects.module.css";
 import themes from "../components/Theme.module.css";
 import exCircle from "../assets/exclamation-circle.svg";
@@ -33,69 +34,53 @@ import tcIncorrect from "../assets/tcIncorrect.png";
 import tcLevelSelect from "../assets/tcLevelSelect.png";
 import tcHint from "../assets/tcHint.png";
 import tcTutorial from "../assets/tcTutorial.png";
-const Projects = () => {
-  const handleScrollTo = (id) => {
-    const el = document.getElementById(id);
-    if (el) {
-      el.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
 
-  return (
-    <div className={styles.Projects} id="pageTop">
+function Projects() {
+  const [activeKey, setActiveKey] = useState('greenDigital');
+  const projCardRef = useRef(null);
 
-      <div className={themes.sectionCard + ' ' + styles.projSection}>
-              <button onClick={() => handleScrollTo('pageTop')} style={{ cursor: 'pointer' }} className={styles.topBtn}>
-        <i class="bi bi-chevron-up"></i>
-      </button>
-        <h1 className={themes.sectionTitle}>Projects</h1>
-        <div className={styles.projCards}>
-          <div className={styles.projCard} onClick={() => handleScrollTo('greenDigital')} style={{ cursor: 'pointer' }}>
-            <h2 className={styles.projName}>Green Digital Twin</h2>
-            <img src={gTwinImg} alt="Digital Twin Thumbnail" className={styles.projThumbnail} />
-            <p className={styles.projDesc}>A multimedia project intended to encourage interest in local green spaces. A physical map was linked with an online app through an NFC card and Arduino components.</p>
-          </div>
-          <div className={styles.projCard} onClick={() => handleScrollTo('rhythmGame')} style={{ cursor: 'pointer' }}>
-            <h2 className={styles.projName}>Rhythm Game</h2>
-            <img src={rhythmMenu} alt="Rhythm Thumbnail" className={styles.projThumbnail} />
-            <p className={styles.projDesc}>A rhythm game created with Unity, using C#. Developed as my final year project at university, focusing on helping players improve their overall rhythm abilities.</p>
-          </div>
-          <div className={styles.projCard} onClick={() => handleScrollTo('takingCharge')} style={{ cursor: 'pointer' }}>
-            <h2 className={styles.projName}>Taking Charge</h2>
-            <img src={tcmenu} alt="Taking Charge Thumbnail" className={styles.projThumbnail} />
-            <p className={styles.projDesc}> A web app developed to support GCSE students when learning about the circuits topic in physics.</p>
-          </div>
+  const projects = [
+    {
+      key: 'greenDigital',
+      title: (
+        <div className={styles.projCard1} style={{ cursor: 'pointer' }} as>
+          <h2 className={styles.projName}>QM HabitApp</h2>
+          <img src={gTwinImg} alt="Digital Twin Thumbnail" className={styles.projThumbnail} />
+          <p className={styles.projDesc}><span className={themes.bold}>Encouraging local environmental interest through an interactive user experience.</span></p>
+         <p className={styles.projDesc}>A physical map was linked with an online app through an NFC card and Arduino components.</p>
         </div>
-      </div>
-      <div className={themes.sectionCard} id="greenDigital">
-        <div className={styles.project}>
-          <div className={styles.projIntro}>
-            <h1 className={styles.projTitle} >Green Digital Twin</h1>
-            <p className={styles.introTxt}>Brief: Group project to create a “Green Digital Twin” that will help to improve people’s awareness, understanding and stewardship of the green spaces around them.</p>
-            <div className={styles.skills}>
-              <div className={styles.skill}>Arduino</div>
-              <div className={styles.skill}>HTML</div>
-              <div className={styles.skill}>CSS</div>
-              <div className={styles.skill}>JavaScript</div>
-              <div className={styles.skill}>Figma</div>
+      ),
+      content: (
+        <div className={styles.proj}>
+        <div className={themes.sectionCard}>
+          <div className={styles.project}>
+            <div className={styles.projIntro}>
+              <h1 className={styles.projTitle} >QM HabitApp</h1>
+              <p className={styles.introTxt}>Brief: Group project to create a “Green Digital Twin” that will help to improve people’s awareness, understanding and stewardship of the green spaces around them.</p>
+              <div className={styles.skills}>
+                <div className={styles.skill}>Arduino</div>
+                <div className={styles.skill}>HTML</div>
+                <div className={styles.skill}>CSS</div>
+                <div className={styles.skill}>JavaScript</div>
+                <div className={styles.skill}>Figma</div>
+              </div>
+            </div>
+            <div className={styles.infoCard}>
+              <img src={exCircle} alt="Exclamation Circle" className={styles.exclamationIcon} />
+              <p className={styles.infoTxt}>A Digital Twin is virtual representation of an object or system that’s updated from real-time data, and uses simulation to help decision making</p>
+            </div>
+            <div className={styles.gdtIntroImg}>
+              <div className={styles.spacer}></div>
+              <div className={styles.infoCard2 + ' ' + styles.gdtInfoCard2Txt}>
+                <p className={styles.infoTxt}>Taking inspiration from games like Pokémon Go and crowd databases like iNaturalist, we decided to focus on creating a community database of the biodiversity in the area. We decided to create an application that would store and show on a map what images people had taken of flora and fauna. To introduce the users to this application we would have physical maps with NFC areas that would take users to the web app when scanned.</p>
+                <p className={styles.infoTxt}> My responsibilities in this project were the digital Design and Front-End development. In addition to this I also supported the other members with physical construction and back-end development.</p>
+              </div>
+              <img src={gTwinEnviro} alt="Green Digital Twin" className={styles.gdtEnviro} />
+              <div className={styles.spacer}></div>
             </div>
           </div>
-          <div className={styles.infoCard}>
-            <img src={exCircle} alt="Exclamation Circle" className={styles.exclamationIcon} />
-            <p className={styles.infoTxt}>A Digital Twin is virtual representation of an object or system that’s updated from real-time data, and uses simulation to help decision making</p>
-          </div>
-          <div className={styles.gdtIntroImg}>
-            <div className={styles.spacer}></div>
-            <div className={styles.infoCard2 + ' ' + styles.gdtInfoCard2Txt}>
-              <p className={styles.infoTxt}>Taking inspiration from games like Pokémon Go and crowd databases like iNaturalist, we decided to focus on creating a community database of the biodiversity in the area. We decided to create an application that would store and show on a map what images people had taken of flora and fauna. To introduce the users to this application we would have physical maps with NFC areas that would take users to the web app when scanned.</p>
-              <p className={styles.infoTxt}> My responsibilities in this project were the digital Design and Front-End development. In addition to this I also supported the other members with physical construction and back-end development.</p>
-            </div>
-            <img src={gTwinEnviro} alt="Green Digital Twin" className={styles.gdtEnviro} />
-            <div className={styles.spacer}></div>
-          </div>
         </div>
-      </div>
-      <div className={themes.sectionCard}>
+         <div className={themes.sectionCard}>
         <h1 className={themes.sectionTitle}>Research</h1>
         <p className={styles.sectionTxt}>This project was based around the biodiversity of our campus greenspaces, so this is where we conducted user engagement and education research.</p>
         <p className={styles.sectionTxtEmphasis}>User research was completed to see whether people would use their phones to scan prompts and to gauge general knowledge on biodiversity.</p>
@@ -134,28 +119,43 @@ const Projects = () => {
           </div>
         </div>
       </div>
-      <div className={themes.sectionCard} id="rhythmGame">
-        <div className={styles.project} >
-          <div className={styles.projIntro}>
-            <h1 className={styles.projTitle} >Rhythm Game</h1>
-            <p className={styles.introTxt}>For my university final year project I created a rhythm game in unity, using a unity store asset that allowed my to import and control MIDI music files. I also did some research into differences in how musicians and non-musicians approach these kinds of games by sending out a questionnaire to my playtesters.</p>
-            <div className={styles.skills}>
-              <div className={styles.skill}></div>
-              <div className={styles.skill}>Unity</div>
-              <div className={styles.skill}>C#</div>
-              <div className={styles.skill}></div>
+      </div>
+      )
+    },
+    {
+      key: 'rhythmGame',
+      title: (
+        <div className={styles.projCard1} style={{ cursor: 'pointer' }}>
+          <h2 className={styles.projName}>Rhythm Game</h2>
+          <img src={rhythmMenu} alt="Rhythm Thumbnail" className={styles.projThumbnail} />
+          <div className={styles.projTxt}>
+          <p className={styles.projDesc}><span className={themes.bold}>A rhythm game created with Unity, using C#.</span></p>
+           <p className={styles.projDesc}>Developed as my final year project at university, focusing on helping players improve their overall rhythm abilities.</p>
+        </div>
+        </div>
+      ),
+      content: (
+                <div className={styles.proj}>
+
+        <div className={themes.sectionCard}>
+          <div className={styles.project}>
+            <div className={styles.projIntro}>
+              <h1 className={styles.projTitle} >Rhythm Game</h1>
+              <p className={styles.introTxt}>For my university final year project I created a rhythm game in unity, using a unity store asset that allowed my to import and control MIDI music files. I also did some research into differences in how musicians and non-musicians approach these kinds of games by sending out a questionnaire to my playtesters.</p>
+              <div className={styles.skills}>
+                <div className={styles.skill}></div>
+                <div className={styles.skill}>Unity</div>
+                <div className={styles.skill}>C#</div>
+                <div className={styles.skill}></div>
+              </div>
             </div>
           </div>
+          <p className={styles.imgTxt}>Click below to play the game!</p>
+          <a href="https://r-truesdale.github.io/Rhythm/final/index.html" target='_blank' rel="noopener noreferrer">
+            <img src={rhythmMenu} alt="Rhythm Game Menu" className={styles.rhythmMenuImg} />
+          </a>
         </div>
-        {/* <div className={styles.infoCard2}>
-          <p className={styles.infoTxt}>For my university final year project I created a rhythm game in unity, using a unity store asset that allowed my to import and control MIDI music files. I also did some research into differences in how musicians and non-musicians approach these kinds of games by sending out a questionnaire to my playtesters.</p>
-        </div> */}
-        <p className={styles.imgTxt}>Click below to play the game!</p>
-        <a href="https://r-truesdale.github.io/Rhythm/final/index.html" target='_blank' rel="noopener noreferrer">
-          <img src={rhythmMenu} alt="Rhythm Game Menu" className={styles.rhythmMenuImg} />
-        </a>
-      </div>
-      <div className={themes.sectionCard}>
+              <div className={themes.sectionCard}>
         <h1 className={themes.sectionTitle}>Game Design</h1>
         <p className={styles.sectionTxt}>In order to review player behavior I created two different play modes, a Game mode and a Practice mode. To keep the game interesting, I made sure to include three difficulty levels for per song.</p>
         <div className={styles.rhythmModes}>
@@ -196,25 +196,40 @@ const Projects = () => {
           <img src={pracMenu} alt="Practice Menu" className={styles.pracMenu} />
         </div>
       </div>
-      <div className={themes.sectionCard} id="takingCharge">
-        <div className={styles.project}>
-          <div className={styles.projIntro}>
-            <h1 className={styles.projTitle} >Taking Charge</h1>
-            <p className={styles.introTxt}>Brief: Group project to design and implement an educational application with a controllable environment.</p>
-            <div className={styles.skills}>
-              <div className={styles.skill}>HTML</div>
-              <div className={styles.skill}>CSS</div>
-              <div className={styles.skill}>JavaScript</div>
-              <div className={styles.skill}>Figma</div>
             </div>
-          </div>
-          <div className={styles.infoCard2}>
-            <p className={styles.infoTxt}>A circuit building simulator focusing on GCSE level content. Intended to be used in conjunction with teachers as an engaging classroom resource or revision tool.</p>
-          </div>
-          <img src={tcmenu} alt="Taking Charge Menu" className={styles.tcMenu} />
+
+      )
+    },
+    {
+      key: 'takingCharge',
+      title: (
+        <div className={styles.projCard1} style={{ cursor: 'pointer' }}>
+          <h2 className={styles.projName}>Taking Charge</h2>
+          <img src={tcmenu} alt="Taking Charge Thumbnail" className={styles.projThumbnail} />
+          <p className={styles.projDesc}><span className={styles.bold}>A web app developed to support GCSE students when learning about the circuits topic in physics.</span></p>
         </div>
-      </div>
-      <div className={themes.sectionCard}>
+      ),
+      content: (
+       <div className={styles.proj}>
+        <div className={themes.sectionCard}>
+          <div className={styles.project}>
+            <div className={styles.projIntro}>
+              <h1 className={styles.projTitle} >Taking Charge</h1>
+              <p className={styles.introTxt}>Brief: Group project to design and implement an educational application with a controllable environment.</p>
+              <div className={styles.skills}>
+                <div className={styles.skill}>HTML</div>
+                <div className={styles.skill}>CSS</div>
+                <div className={styles.skill}>JavaScript</div>
+                <div className={styles.skill}>Figma</div>
+              </div>
+            </div>
+            <div className={styles.infoCard2}>
+              <p className={styles.infoTxt}>A circuit building simulator focusing on GCSE level content. Intended to be used in conjunction with teachers as an engaging classroom resource or revision tool.</p>
+            </div>
+            <img src={tcmenu} alt="Taking Charge Menu" className={styles.tcMenu} />
+          </div>
+        </div>
+        <div className={themes.sectionCard}>
         <h1 className={themes.sectionTitle}>Design</h1>
         <p className={styles.sectionTxt}>This project idea was reached from reaching out to GCSE aged people and asking which subjects they needed more help understanding or which was their least favourite subject and why. We also asked these questions to people who had completed their GCSEs for additional responses. Physics was a common problem subject with people saying they had trouble visualizing certain concepts.</p>
         <p className={styles.sectionTxtEmphasis}>Prototypes and storyboards were iterated upon until we reached our final high fidelity prototype. I have shown the progression of the design process below.</p>
@@ -242,6 +257,43 @@ const Projects = () => {
         </div>
       </div>
     </div >
-  )
+      )
+    }
+  ];
+
+  const handleTabSelect = (key) => {
+    setActiveKey(key);
+    setTimeout(() => {
+      if (projCardRef.current) {
+        projCardRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }, 0);
+  };
+
+  return (
+    <div className={styles.projSection}>
+      {/* <div className={themes.sectionCard}> */}
+      <div ref={projCardRef} />
+      <Tabs
+        activeKey={activeKey}
+        onSelect={handleTabSelect}
+        className={styles.projCards}
+        mountOnEnter
+        unmountOnExit
+      >
+        {projects.map((proj) => (
+          <Tab
+            eventKey={proj.key}
+            key={proj.key}
+            title={proj.title}
+            tabClassName={styles.projCard}
+          >
+            {proj.content}
+          </Tab>
+        ))}
+      </Tabs>
+    </div>
+  );
 }
+
 export default Projects;
